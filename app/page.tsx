ad6554ba3ef03a594e3bea6596af40e93f81e75c
bibarys <bibaryssultan@icloud.com>
2025-02-03 01:13:18 +0500

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="font-mono text-3xl text-white">Welcome to the world</h1>
-  );
+  return <h1 className="background-light850_dark100">Welcome to the world</h1>;
 }
