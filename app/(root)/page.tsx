6f478a1bb6ea3ee0913315d31ad3a77ae2eb9af7
bibarys <bibaryssultan@icloud.com>
2025-02-09 00:08:32 +0500

    All Questions (Home PAge)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
+
 export default async function Home() {
   return (
     <>
-      <h1 className="background-light850_dark100">Welcome to the world</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="text-dark100_light900 h1-bold">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Search</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 }
