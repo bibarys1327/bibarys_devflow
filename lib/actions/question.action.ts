58d2800a787f3829f0b09fc085ee4dc6f10e58f1
bibarys <bibaryssultan@icloud.com>
2025-02-21 02:37:45 +0500

    Fetch Real Question Details

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -17,6 +17,12 @@ import mongoose, { FilterQuery } from "mongoose";
 import Question, { IQuestionDoc } from "@/database/question.model";
 import Tag, { ITagDoc } from "@/database/tag.model";
 import TagQuestion from "@/database/tag-question.model";
+import {
+  CreateQuestionParams,
+  EditQuestionParams,
+  GetQuestionParams,
+  GetQuestionParams,
+} from "@/types/action";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -194,7 +200,9 @@ export async function getQuestion(
   const { questionId } = validationResult.params!;
 
   try {
-    const question = await Question.findById(questionId).populate("tags");
+    const question = await Question.findById(questionId)
+      .populate("tags")
+      .populate("author", "_id name image");
 
     if (!question) {
       throw new Error("Question not found");
