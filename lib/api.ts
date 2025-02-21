f02f43d553e35bc1538c395c928fcf98d545553e
bibarys <bibaryssultan@icloud.com>
2025-02-22 03:47:26 +0500

    AI Answer API Route

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,8 @@ import { IUser } from "@/database/user.model";
 import { fetchHandler } from "./handlers/fetch";
 import { IAccount } from "@/database/account.model";
 import ROUTES from "@/constants/routes";
+import { SignInWithOAuthParams } from "@/types/action";
+import { APIResponse } from "@/types/global";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
@@ -64,5 +66,13 @@ export const api = {
       fetchHandler(`${API_BASE_URL}/accounts/${id}`, {
         method: "DELETE",
       }),
+  },
+  ai: {
+    getAnswer: (question: string, content: string): APIResponse<string> => {
+      fetchHandler(`${API_BASE_URL}/ai/answers`, {
+        method: "POST",
+        body: JSON.stringify({ question, content }),
+      });
+    },
   },
 };
